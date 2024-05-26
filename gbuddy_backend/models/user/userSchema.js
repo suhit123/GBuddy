520f30a609bb01941a7b7808d8fae41670eacb79
Gowtham Raj <gmanda@gitam.in>
2024-05-26 07:46:02 +0530

    User Authentication

--- gbuddy_backend/models/user/userSchema.js
+++ gbuddy_backend/models/user/userSchema.js
@@ -13,13 +13,7 @@ const usersSchema = mongoose.Schema({
     campus : {type : String , required : false } , 
     branch : {type : String , required : false} , 
     year : {type : Number , required : false}, 
-    rollnumber : {type : String , required : false} , 
-    linkedin : {type : String , required : false} , 
-    github : {type : String , required : false} , 
-    leetcode : {type : String , required : false} , 
-    semester : {type : Number , required : false} ,
-    phone : {type : String , required : false},
-    SavedFiles : {type : Array , required : false}
+    rollnumber : {type : String , required : false} 
 });
 
 module.exports = mongoose.model('users',usersSchema);
